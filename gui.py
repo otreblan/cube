80fce379e32ba1c337e1fad524f84e0ffaccf4d0
otreblan <otreblain@gmail.com>
2021-06-24 11:08:24 -0500

    Rotación

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from sys import stdin
-from pyrr import Vector3, geometry
+from pyrr import Vector3, geometry, matrix33
 from typing import List, Tuple, Any
 
 import matplotlib.pyplot as plt
@@ -15,12 +15,23 @@ def plot_edges(data: List[Tuple[Vector3, Vector3]]) -> None:
     print(data)
 
 
+def rotate(data: List[Vector3], rotation: np.ndarray) -> List[Vector3]:
+    return [
+        Vector3(matrix33.apply_to_vector(vec=v, mat=rotation))
+        for v in data
+    ]
+
+
 def plot_cube(cube: Tuple[np.ndarray, Any], rotation: Vector3) -> None:
+    rot_mat = matrix33.create_from_eulers(rotation)
+
+    # Rotación
+    rotated = rotate([Vector3(v) for v in cube[0]], rot_mat)
+
     # TODO Proyección
-    # TODO Rotación
 
     # Vértices
-    plot_vertex([Vector3(v) for v in cube[0]])
+    plot_vertex(rotated)
 
     # TODO Aristas
 
