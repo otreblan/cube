abacdec71548521af677bf5f5a3c2ad5964ebb18
otreblan <otreblain@gmail.com>
2021-06-24 08:37:55 -0500

    plot_cube()

--- gui.py
+++ gui.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python3
 
-from pyrr import Vector3
+from sys import stdin
+from pyrr import Vector3, geometry
+from typing import List, Tuple, Any
 
+import matplotlib.pyplot as plt
 import numpy as np
-import sys
+
+def plot_vertex(data: List[Vector3]) -> None:
+    for vertex in data:
+        plt.plot(vertex.x, vertex.y, 'bo')
+
+def plot_edges(data: List[Tuple[Vector3, Vector3]]) -> None:
+    print(data)
 
 
-# TODO Esto debería leer un ángulo de euler y dibujarlo.
-def updatePlot(data: Vector3) -> None:
-    print(data, end='')
+def plot_cube(cube: Tuple[np.ndarray, Any], rotation: Vector3) -> None:
+    # TODO Proyección
+    # TODO Rotación
 
+    # Vértices
+    plot_vertex([Vector3(v) for v in cube[0]])
+
+    # TODO Aristas
+
+def update_plot(data: Vector3) -> None:
+    plot_cube(geometry.create_cube(), data)
+
+    plt.show()
 
 def main() -> None:
-    for line in sys.stdin:
-        updatePlot(np.fromstring(line, count = 3, sep=','))
+    for line in stdin:
+        update_plot(Vector3(np.fromstring(line, count = 3, sep=',')))
 
 
 if __name__ == '__main__':
